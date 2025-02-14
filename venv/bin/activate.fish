9f35533910747074952d714be273f3012ba884fe
Mamy <mamitianarandrianantenaina253@gmail.com>
2025-02-14 13:17:43 +0300

    create virtual environnement

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/miekely/Bureau/tkinter_test/venv
+set -gx VIRTUAL_ENV /home/miekely/Bureau/PROJET_GROUP3/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
